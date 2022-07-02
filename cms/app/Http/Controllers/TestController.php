696174442673ab4600944001f1d34b8ccc2fe522
Walter <walterrjr.86@gmail.com>
2022-07-02 13:29:47 -0300

    aula-1-instalado o projeto em laravel 7, instalado o pacote npm, instalado o vue@2.7.0, instalado o vue-router@3.1.6

---
+++ cms/app/Http/Controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function controllerMethod(){
+        return response()->json([
+            'msg' => "we should return only json"
+        ]);
+    }
+}
